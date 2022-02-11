b0b336f113b30b4db304d466d4b885b3b3c8366f
Tomasgaviria <73371810+Tomasgaviria@users.noreply.github.com>
2022-02-11 11:56:06 -0500

    Update WarmUp.hs

--- src/WarmUp.hs
+++ src/WarmUp.hs
@@ -2,9 +2,9 @@ module Main where
 import Numeric.Natural
 
  main :: IO ()
-fac n = if n == 0 
+fac1 n = if n == 0 
            then 1
-           else n * fac (n-1)
+           else n * fac1 (n-1)
 
 fac2 0 = 1
 fac2 n = n * fac2 (n-1)
